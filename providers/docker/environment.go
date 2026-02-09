bf9f2a4daa2e32c2cb5de7cc270568b3899e6671
Steven Cartmell <45599587+ruffel@users.noreply.github.com>
2026-02-09 18:51:14 +0000

    fix(environment): Standardize `Run` behaviour to return `Result`

--- providers/docker/environment.go
+++ providers/docker/environment.go
@@ -53,11 +53,13 @@ func (e *Environment) Run(ctx context.Context, cmd *invoke.Command) (*invoke.Res
 
 	defer func() { _ = proc.Close() }()
 
-	if err := proc.Wait(); err != nil {
-		return nil, err
+	waitErr := proc.Wait()
+	// Always return the result if available, even if Wait failed (e.g. non-zero exit code)
+	if res := proc.Result(); res != nil {
+		return res, waitErr
 	}
 
-	return proc.Result(), nil
+	return nil, waitErr
 }
 
 // Start spawns a command asynchronously.
